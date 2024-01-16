fa2a67c879a31b9d6c1e1b69c08aacfb946b3437
Mickael Riss <mendos.prod@gmail.com>
2024-01-15 19:42:01 -0500

    first commit

---
+++ src/components/NavLink.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+	name: 'NavLink',
+  props: ['url', 'text']
+}
+</script>
+
+<template>
+	<a v-bind:href="url">{{ text }}</a>
+</template>
+
+<style>
+	a {
+		text-decoration: none;
+	}
+</style>
